bf2999c5df2c2723d7919a2911fef44df9745017
Logan Ricard <logvnjs@gmail.com>
2022-05-10 11:27:12 -0500

    updated footer with content

--- components/navigation/Footer.tsx
+++ components/navigation/Footer.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import classes from '../../styles/navigation/Footer.module.css';
+import ArrowBtn from '../buttons/ArrowBtn';
 
 const Footer = () => {
   return (
@@ -8,6 +9,21 @@ const Footer = () => {
       <div className={classes.title}>
         <img src="/assets/shared/desktop/logo-light.svg" alt="Photosnap" />
       </div>
+      <div className={classes.social}>
+        <img src="/assets/shared/desktop/facebook.svg" alt="Facebook" />
+        <img src="/assets/shared/desktop/youtube.svg" alt="Youtube" />
+        <img src="/assets/shared/desktop/twitter.svg" alt="Twitter" />
+        <img src="/assets/shared/desktop/pinterest.svg" alt="Pinterest" />
+        <img src="/assets/shared/desktop/instagram.svg" alt="Instagram" />
+      </div>
+      <div className={classes.navigation}>
+        <h4>home</h4>
+        <h4>stories</h4>
+        <h4>features</h4>
+        <h4>pricing</h4>
+      </div>
+      <ArrowBtn color="white" />
+      <p className={classes.copyright}>copyright 2019. all right reserved</p>
     </footer>
   );
 };
