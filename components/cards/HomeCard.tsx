372e2cd2f4e83416bda443031557039a8dc914a5
Logan Ricard <logvnjs@gmail.com>
2022-05-13 16:09:16 -0500

    redesigned HomeCard UI logic

--- components/cards/HomeCard.tsx
+++ components/cards/HomeCard.tsx
@@ -32,19 +32,99 @@ const HomeCard: FC<Props> = ({
   photoPosition,
 }) => {
   const [picture, setPicture] = useState<StaticImageData>(photoM);
+  const [altLayout, setAltLayout] = useState<boolean>(false);
   const { screenWidth } = useStore();
 
   useEffect(() => {
     console.log(screenWidth);
     if (screenWidth >= 1280) {
+      setAltLayout(true);
       setPicture(photoL);
     } else if (screenWidth >= 768 && screenWidth < 1280) {
+      setAltLayout(true);
       setPicture(photoM);
     } else {
-      setPicture(photoS);
+      setAltLayout(false);
     }
   }, [screenWidth]);
 
+  return (
+    <>
+      {!altLayout && (
+        <Mobile
+          photo={photoS}
+          name={name}
+          accent={accent}
+          button={button}
+          color={color}
+          title={title}
+          info={info}
+        />
+      )}
+      {altLayout && (
+        <Desktop
+          photoPosition={photoPosition}
+          photo={picture}
+          name={name}
+          accent={accent}
+          button={button}
+          color={color}
+          title={title}
+          info={info}
+        />
+      )}
+    </>
+  );
+};
+
+interface SubProps {
+  photo: StaticImageData;
+  accent: boolean;
+  color: string;
+  title: string;
+  info?: string;
+  photoPosition?: string;
+  name: string;
+  button: boolean;
+}
+
+const Mobile: FC<SubProps> = ({
+  photo,
+  name,
+  accent,
+  button,
+  color,
+  title,
+  info,
+}) => {
+  return (
+    <section className={classes.container}>
+      <div className={classes['image-container']}>
+        <Image layout="responsive" objectFit="contain" src={photo} alt={name} />
+      </div>
+      <div className={classes[`content-container__${color}`]}>
+        <div className={classes['text-container']}>
+          {accent && <div className={classes.accent}></div>}
+          <h2 className={classes.title}>{title}</h2>
+          {info && <p className={classes.info}>{info}</p>}
+          {button && <ArrowBtn title="get an invite" color={color} />}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+const Desktop: FC<SubProps> = ({
+  photo,
+  name,
+  accent,
+  button,
+  color,
+  title,
+  info,
+  photoPosition,
+}) => {
+  const { screenWidth } = useStore();
   return (
     <section
       style={
@@ -55,16 +135,11 @@ const HomeCard: FC<Props> = ({
       className={classes.container}
     >
       <div className={classes['image-container']}>
-        <Image
-          layout="responsive"
-          objectFit="contain"
-          src={picture}
-          alt={name}
-        />
+        <Image layout="responsive" objectFit="contain" src={photo} alt={name} />
       </div>
       <div className={classes[`content-container__${color}`]}>
-        {accent && <div className={classes.accent}></div>}
         <div className={classes['text-container']}>
+          {accent && <div className={classes.accent}></div>}
           <h2 className={classes.title}>{title}</h2>
           {info && <p className={classes.info}>{info}</p>}
           {button && <ArrowBtn title="get an invite" color={color} />}
