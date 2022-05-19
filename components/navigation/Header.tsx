93562a13278c926afd13581fa85f28f32f41c3ad
Logan Ricard <logvnjs@gmail.com>
2022-05-19 11:11:07 -0500

    added Framer Motion to Mobile Nav

--- components/navigation/Header.tsx
+++ components/navigation/Header.tsx
@@ -1,6 +1,7 @@
 import { useState, FC } from 'react';
 import { Pivot as Hamburger } from 'hamburger-react';
 import Link from 'next/link';
+import { motion } from 'framer-motion';
 
 import classes from '../../styles/navigation/Header.module.css';
 
@@ -37,22 +38,27 @@ const Header: FC = () => {
           <div className={classes.btn}>get an invite</div>
         </div>
       </nav>
-      {menuToggle && (
-        <section className={classes['menu-container']}>
-          <div className={classes.navigation}>
-            <Link href="/stories">
-              <h4 onClick={close}>stories</h4>
-            </Link>
-            <Link href="/features">
-              <h4 onClick={close}>features</h4>
-            </Link>
-            <Link href="/pricing">
-              <h4 onClick={close}>pricing</h4>
-            </Link>
-          </div>
-          <div className={classes.btn}>get an invite</div>
-        </section>
-      )}
+      <motion.div
+        transition={{ layout: { duration: 0.75, type: 'spring' } }}
+        layout
+      >
+        {menuToggle && (
+          <motion.section className={classes['menu-container']}>
+            <div className={classes.navigation}>
+              <Link href="/stories">
+                <h4 onClick={close}>stories</h4>
+              </Link>
+              <Link href="/features">
+                <h4 onClick={close}>features</h4>
+              </Link>
+              <Link href="/pricing">
+                <h4 onClick={close}>pricing</h4>
+              </Link>
+            </div>
+            <div className={classes.btn}>get an invite</div>
+          </motion.section>
+        )}
+      </motion.div>
     </>
   );
 };
